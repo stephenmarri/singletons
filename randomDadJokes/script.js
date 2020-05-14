a95bbed2b483eb3b675d68413fe15bf09bf5267f
Marri <stephenmarri@kpmg.com>
2020-05-14 21:28:57 +0530

    added two more apis

--- randomDadJokes/script.js
+++ randomDadJokes/script.js
@@ -1,10 +1,21 @@
 window.addEventListener('load',()=>{
     const jokeEl = document.getElementById('joke');
     const jokeBtn = document.getElementById('get_joke');
+    const tag = document.getElementById('tag');
+    var apis = [generateDadJoke,generateNorrisJoke,generateGithubJoke]
 
-    jokeBtn.addEventListener('click',generateJoke);
-    generateJoke();
-    async function generateJoke(){
+    jokeBtn.addEventListener('click',()=>{
+        let pick = Math.floor(Math.random()*(apis.length));
+        apis[pick]();
+        
+    });
+    generateDadJoke();
+
+
+
+
+    
+    async function generateDadJoke(){
         console.log('starting fetch');
         const jokeFromAPI = await fetch('https://icanhazdadjoke.com/',{
             headers : {
@@ -14,6 +25,24 @@ window.addEventListener('load',()=>{
 
         const joke = await jokeFromAPI.json();
         jokeEl.textContent = joke.joke;
+        tag.textContent="[Dad Jokes]";
+
+    };
+
+    async function generateNorrisJoke(){
+        console.log('starting fetch');
+        const jokeFromAPI = await fetch('https://api.chucknorris.io/jokes/random');
+        const joke = await jokeFromAPI.json();
+        jokeEl.textContent = joke.value;
+        tag.textContent="[Chuck Norris Jokes]";
+
+    };
+    async function generateGithubJoke(){
+        console.log('starting fetch');
+        const jokeFromAPI = await fetch('https://official-joke-api.appspot.com/random_joke');
+        const joke = await jokeFromAPI.json();
+        jokeEl.textContent = joke.setup +" " + joke.punchline;
+        tag.textContent="[Random Jokes]";
 
     };
 
